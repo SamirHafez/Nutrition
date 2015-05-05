ca070375f47edd03e29833ea24e53c19e19b688e
Samir Hafez <samir.hafez@xpand-it.com>
2015-05-05 10:30:32 +0100

    Added InfoViewModel properties Added deisgn data for WP project

--- src/Nutrition/Nutrition.Core/ViewModels/InfoViewModel.cs
+++ src/Nutrition/Nutrition.Core/ViewModels/InfoViewModel.cs
@@ -12,9 +12,59 @@ namespace Nutrition.Core.ViewModels
     {
         NutritionTable nutritionTable;
 
+        double kcals;
+        public double KCals
+        {
+            get { return kcals; }
+            set { kcals = value; RaisePropertyChanged(() => KCals); }
+        }
+
+        double carbPercentage;
+        public double CarbPercentage
+        {
+            get { return carbPercentage; }
+            set { carbPercentage = value; RaisePropertyChanged(() => CarbPercentage); }
+        }
+
+        double proteinPercentage;
+        public double ProteinPercentage
+        {
+            get { return proteinPercentage; }
+            set { proteinPercentage = value; RaisePropertyChanged(() => ProteinPercentage); }
+        }
+
+        double fatPercentage;
+        public double FatPercentage
+        {
+            get { return fatPercentage; }
+            set { fatPercentage = value; RaisePropertyChanged(() => FatPercentage); }
+        }
+
         protected override void InitFromBundle(IMvxBundle parameters)
         {
-            nutritionTable = parameters.Read<NutritionTable>();
+            UpdateTable(parameters.Read<NutritionTable>());
+        }
+
+        void UpdateTable(NutritionTable nutritionTable)
+        {
+            KCals =
+                nutritionTable.Carbs * 4 +
+                nutritionTable.Protein * 4 +
+                nutritionTable.Fat * 9;
+
+            CarbPercentage =
+                (nutritionTable.Carbs /
+                (nutritionTable.Carbs + nutritionTable.Protein + nutritionTable.Fat)) * 100;
+
+            ProteinPercentage =
+                (nutritionTable.Protein /
+                (nutritionTable.Carbs + nutritionTable.Protein + nutritionTable.Fat)) * 100;
+
+            FatPercentage =
+                (nutritionTable.Fat /
+                (nutritionTable.Carbs + nutritionTable.Protein + nutritionTable.Fat)) * 100;
+
+            this.nutritionTable = nutritionTable;
         }
     }
 }
